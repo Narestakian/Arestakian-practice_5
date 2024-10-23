5aec51fe43f9705df5352592aae7b34e9e5f07da
narestakian <n.arestakian@gmail.com>
2024-10-23 21:53:48 +0400

    task_1_Nino

---
+++ src/test/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main (String[] args){
+        System.out.println("Task1");
+        isEven check = new isEven();
+        check.isEven(4);
+        check.isEven(7);
+
+    }
+}
---
+++ src/test/java/isEven.java
@@ -0,0 +1,11 @@
+public class isEven {
+     public int isEven(int a) {
+        if (a % 2 == 0) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
+        return a;
+
+    }
+}
